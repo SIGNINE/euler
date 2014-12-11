c4583dbdf5ccbc270568269ec64d91e45e9e042a
Shamil Ishraq <shamil.ishraq@gmail.com>
2014-12-11 15:07:35 -0500

    5 completed

--- 5.rb
+++ 5.rb
@@ -4,11 +4,21 @@
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 =end
 
+def lcm n1, n2
+  min = [n1, n2].min
+  max = [n1, n2].max
+
+  (2..min).each do |i|
+    p = max * i
+    return p if p % min == 0
+  end
+end
+
 def smallest_multiple limit
-  num = limit 
-  (2..limit-1).each { |i| num = num * i if num % i != 0 }
+  num = 2 
+  (2..limit-1).each { |i| num = lcm(num, i)  if num % i != 0 }
   
   num
 end
 
-puts smallest_multiple 10 
+puts smallest_multiple 20
